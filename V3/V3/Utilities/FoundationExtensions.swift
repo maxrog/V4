ed94335657dd5f3d37a23abfdfd1b83b7f0640a0
Max Rogers <max.rogers@yinzcam.com>
2022-09-29 19:48:04 -0400

    Initial commit with Xcode project

---
+++ V3/V3/Utilities/FoundationExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  FoundationExtensions.swift
+//  V3
+//
+//  Created by Max Rogers on 9/29/22.
+//
+
+import Foundation
---
+++ V3/V3/Utilities/ViewExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ViewExtensions.swift
+//  V3
+//
+//  Created by Max Rogers on 9/29/22.
+//
+
+import SwiftUI
