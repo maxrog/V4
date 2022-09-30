99db6b1f0ef8531585ba459a4aea4f53f4aa4a11
Max Rogers <max.rogers@yinzcam.com>
2022-09-29 20:16:02 -0400

    Fix button

--- V3/V3/Views/V3Views/V3Button.swift
+++ V3/V3/Views/V3Views/V3Button.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct V3Button: View {
     let title: String
+    let action: () -> Void
     var body: some View {
-        V3Button(title: title)
+        Button(title, action: action)
             .buttonStyle(.plain)
     }
 }
 
 struct V3Button_Previews: PreviewProvider {
     static var previews: some View {
-        V3Button(title: "Test Button")
+        V3Button(title: "Test Button") {
+            print("Test Button Pressed!")
+        }
     }
 }
