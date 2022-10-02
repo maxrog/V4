549117be85783ce3cab7ef101c56f52a18f2b7d1
Max Rogers <max.rogers@yinzcam.com>
2022-10-02 10:15:51 -0400

    color hex init

--- V4/V4/Utilities/Extensions/FoundationExtensions.swift
+++ V4/V4/Utilities/Extensions/FoundationExtensions.swift
@@ -119,5 +119,19 @@ public extension Color {
     static let systemRed = Color(UIColor.systemRed)
     static let systemTeal = Color(UIColor.systemTeal)
     static let systemIndigo = Color(UIColor.systemIndigo)
+    
+    
+    // MARK: Init
+    
+    /// Initialized Color with hex value eg 0xFFFFFF
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
+    }
 
 }
